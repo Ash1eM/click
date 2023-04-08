8cf41b0aef9fade937b10faf6abe62a6986505c1
Ash1eM <ashley.morris101@yahoo.com>
2023-04-08 09:01:30 -0400

    done

--- click.js
+++ click.js
@@ -1,10 +1,39 @@
 const main = document.querySelector('main');
-const button = document.querySelector("button");
+const buttons = document.getElementById("Foo");
 
-button.addEventListener('click', responseToClick)
+buttons.addEventListener('click', responseToClick)
 
 function responseToClick() {
-   const hTag= document.createElement('h3');
-    hTag.textContent="Foo";
-    main.appendChild(hTag);
-} 
+    const hTag = document.createElement('h3');
+    hTag.textContent = "Foo"
+    main.appendChild(hTag)
+}
+
+const button = document.getElementById("Bar");
+
+button.addEventListener ('click', myFunction);
+function myFunction() {
+    const anTag =  document.createElement('h3');
+    anTag.textContent = "Bar"
+    main.appendChild(anTag)
+}
+
+const anbutton = document.getElementById("FooBar");
+
+anbutton.addEventListener('click', anFunction);
+function anFunction() {
+    const lastTag = document.createElement('h2');
+    lastTag.textContent = "FooBar"
+    main.appendChild(lastTag)
+}
+
+
+
+
+
+
+
+
+
+    
+
